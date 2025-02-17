7c79a6f41abf9a9a60ec27e3af77415949920e93
Matt Reath <mattreath@icloud.com>
2025-02-16 18:57:38 -0600

    cli optoin parsing

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
-#include <config.h>
-#include <stdio.h>
+#include <glib.h>
 
-int
-main (void)
+static gboolean verbose = FALSE;
+
+int 
+main (int argc, char *argv[])
 {
-  puts ("This is " PACKAGE_STRING ".");
-  return 0;
+    static GOptionEntry options[] = {
+        { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+        { NULL }
+    };
+
+    GError *error = NULL;
+    GOptionContext *context;
+
+    context = g_option_context_new ("- autobase");
+    g_option_context_add_main_entries (context, options, NULL);
+    if (!g_option_context_parse (context, &argc, &argv, &error)) {
+        g_print ("option parsing failed: %s\n", error->message);
+        exit (1);
+    }
+
+    if (verbose) {
+        g_print ("Verbose mode\n");
+    }
+
+    return 0;
 }
